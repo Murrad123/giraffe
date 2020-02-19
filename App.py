07537f1ae9e7e17108ac8b2d7c3e56ddefaee766
murrad asgheddi <murrad@murrads-air.wkstn.qmul.ac.uk>
2020-02-19 11:30:14 +0000

    first commit

---
+++ App.py
@@ -0,0 +1,23 @@
+# name=input('Insert your name:')
+# num=input('Enter a number:')
+# id= name+num
+# print(id)
+
+# *47:
+# num=int(input('Insert a number:'))
+# num2=int(input('Insert another number:'))
+# total=num+num2
+# option = (input('Would you like to add another number:'))
+# while option=='y':
+#     num = int(input('Insert a number:'))
+#     option = (input('Would you like to add another number:'))
+#     total= total+num
+# else: print(total)
+invites=0
+option = input('Would you like to invite anyone to the party?:')
+while: option='y'
+
+
+
+
+
